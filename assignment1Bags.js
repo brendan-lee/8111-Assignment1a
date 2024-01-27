4a97a81dd95dbb7d0ac00063a568e951f82e1407
Brendan Lee <lizheminglzm@gmail.com>
2024-01-26 19:31:29 -0500

    style: reformat

--- assignment1Bags.js
+++ assignment1Bags.js
@@ -1,54 +1,55 @@
-const Order = require("./assignment1Order");
+const Order = require('./assignment1Order')
 
 const OrderState = Object.freeze({
-    WELCOMING:   Symbol("welcoming"),
-    SIZE:   Symbol("size"),
-    COLOUR:   Symbol("Colour"),
-    CLEANER:  Symbol("Cleaner")
-});
+    WELCOMING: Symbol('welcoming'),
+    SIZE: Symbol('size'),
+    COLOUR: Symbol('Colour'),
+    CLEANER: Symbol('Cleaner')
+})
 
-module.exports = class BagsOrder extends Order{
-    constructor(){
-        super();
-        this.stateCur = OrderState.WELCOMING;
-        this.sSize = "";
-        this.sColour = "";
-        this.sCleaner = "";
-        this.sItem = "backbag";
+module.exports = class BagsOrder extends Order {
+    constructor() {
+        super()
+        this.stateCur = OrderState.WELCOMING
+        this.sSize = ''
+        this.sColour = ''
+        this.sCleaner = ''
+        this.sItem = 'backbag'
     }
-    handleInput(sInput){
-        let aReturn = [];
-        switch(this.stateCur){
+
+    handleInput(sInput) {
+        let aReturn = []
+        switch (this.stateCur) {
             case OrderState.WELCOMING:
-                this.stateCur = OrderState.SIZE;
-                aReturn.push("Welcome to Conestoga's Bags.");
-                aReturn.push("What size bag would you like?");
-                break;
+                this.stateCur = OrderState.SIZE
+                aReturn.push('Welcome to Conestoga\'s Bags.')
+                aReturn.push('What size bag would you like?')
+                break
             case OrderState.SIZE:
                 this.stateCur = OrderState.COLOUR
-                this.sSize = sInput;
-                aReturn.push("What Colour would you like?");
-                break;
+                this.sSize = sInput
+                aReturn.push('What Colour would you like?')
+                break
             case OrderState.COLOUR:
                 this.stateCur = OrderState.CLEANER
-                this.sColour = sInput;
-                aReturn.push("Would you like Cleaner with that?");
-                break;
+                this.sColour = sInput
+                aReturn.push('Would you like Cleaner with that?')
+                break
             case OrderState.CLEANER:
-                this.isDone(true);
-                if(sInput.toLowerCase() != "no"){
-                    this.sCleaner = sInput;
+                this.isDone(true)
+                if (sInput.toLowerCase() != 'no') {
+                    this.sCleaner = sInput
                 }
-                aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sColour}`);
-                if(this.sCleaner){
-                    aReturn.push(this.sCleaner);
+                aReturn.push('Thank-you for your order of')
+                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sColour}`)
+                if (this.sCleaner) {
+                    aReturn.push(this.sCleaner)
                 }
-                let d = new Date(); 
-                d.setMinutes(d.getMinutes() + 20);
-                aReturn.push(`Please pick it up at ${d.toTimeString()}`);
-                break;
+                let d = new Date()
+                d.setMinutes(d.getMinutes() + 20)
+                aReturn.push(`Please pick it up at ${d.toTimeString()}`)
+                break
         }
-        return aReturn;
+        return aReturn
     }
 }
